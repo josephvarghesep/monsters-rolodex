1b15bba51631ac357ee24f3619d058cc7f561529
joseph <josephvarghesep@gmail.com>
2020-04-07 22:19:03 +1000

    Updates

---
+++ precache-manifest.a06af3b6493b0894ec91a1ac6ff70396.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b21f58e5807a6b36a409fe05a6ae247",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3e7459513d878d5ee058",
+    "url": "/monsters-rolodex/static/css/main.f5b9e086.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e7459513d878d5ee058",
+    "url": "/monsters-rolodex/static/js/main.97aeeaa4.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
